0fab05f1bd5318fe8c736cf4e42d74157562a136
Vincent Pham <vincentvpham@gmail.com>
2021-01-14 10:24:31 -0800

    3.0.2

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import {
 } from './helpers/constants';
 
 // we track the version this way because importing the package.json causes issues
-export const VERSION = '3.0.1';
+export const VERSION = '3.0.2';
 
 export class WildlinkClient {
   private applicationId: number;
